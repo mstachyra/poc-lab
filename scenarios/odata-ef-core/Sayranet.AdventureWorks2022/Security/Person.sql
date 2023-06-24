8111beaf9784d3377f4ae2d9c38448e3a37743f2
Michał Stachyra <m.stachyra90@gmail.com>
2023-06-25 00:29:09 +0200

    Add DB SqlProj

---
+++ scenarios/odata-ef-core/Sayranet.AdventureWorks2022/Security/Person.sql
@@ -0,0 +1,17 @@
+﻿CREATE SCHEMA [Person]
+    AUTHORIZATION [dbo];
+
+
+
+
+
+
+
+
+
+
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Contains objects related to names and addresses of customers, vendors, and employees', @level0type = N'SCHEMA', @level0name = N'Person';
+
